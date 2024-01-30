171b85c62cab26174af55aca5955f4d123c20e3c
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-01-30 09:30:38 -0600

    Added overview of basic python data types

---
+++ 01_Python_Basics/02_Data_Types.py
@@ -0,0 +1,45 @@
+# Data Types: 
+
+# All values in python belong to a specific data 
+# type: 
+
+# INTEGERS(Ints): Whole numbers like -2 and 40
+
+# FLOATING POINT(Floats): Decimal point numbers like:
+# 3.14 or 2.1
+# Note: 42 is an int, but 42.0 is a float 
+
+# STRINGS: Text values wrapped between single quotation
+# marks are string values like 'Hello World', strings 
+# are also able to be concatenated using the '+' 
+# operator: 
+
+print('Hello' + ' ' + 'World' + '!') # Hello World!
+
+# String replication can be implemented using the 
+# '*' multiplication operand
+
+print('hello' * 3) # hellohellohello
+
+print('Hello' + '!' * 5) # Hello!!!!!
+
+# VARIABLES: Variables are used to assign values: 
+
+name = 'Luke';
+
+print(name) # Luke
+print('Hello' + ' ' + name) # Hello Luke
+
+# Variables can be reassigned: 
+
+name = 'Peter'
+print(name) # Peter
+
+# You can also chain together expressions and operands:
+
+opera = 'Good'
+print(opera) # Good
+
+opera = opera + ' ' + 'day for an opera' 
+print(opera) # Good day for an opera
+
