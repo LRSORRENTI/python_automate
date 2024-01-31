8814ee8442ef863209911237196f121ddd324bd3
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-01-31 08:37:18 -0600

    Added more notes on comparison operators in python, floats and ints 42.0 == 42 does return true

--- 02_Flow_Control/02-Comparison-Operators.py
+++ 02_Flow_Control/02-Comparison-Operators.py
@@ -1,2 +1,53 @@
 # Comparison Operators
 
+# It's important to introduce comparison operators as 
+# they always determine if a given comparison is 
+# true or false 
+
+'''
+== EQUAL TO 
+!= NOT EQUAL TO 
+< LESS THAN 
+> GREATER THAN 
+<= LESS THAN OR EQUAL TO 
+>= GREATER THAN OR EQUAL TO 
+
+'''
+
+print(42 == 42)
+# true 
+
+print(42 == 41)
+# false 
+
+print(42 >= 42)
+# true 
+
+print(42 <= 40)
+# false 
+
+print(42 != 43)
+# true 
+
+print(42 != 42)
+# false 
+
+print(42 > 41)
+# true 
+
+print(42 > 42)
+# false 
+
+print(42 < 43)
+# true
+
+print(42 < 40)
+# false 
+
+# FLOATS and INTS can be equal:
+
+print(42.0 == 42)
+# true 
+
+print(42.1 == 42)
+# false 
