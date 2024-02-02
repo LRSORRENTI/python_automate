34b08e8533defa200df2a745c31c8e0eb75c3c7a
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-02-02 09:56:59 -0600

    added intro to writing functions in python

---
+++ 03_Functions/03_Third_Party_Packages.py
@@ -0,0 +1,8 @@
+import pyperclip
+
+# pyperclip is a very useful package to copy 
+# large amounts of text into a program, as well 
+# as receive text
+
+pyperclip.copy('hello world')
+pyperclip.paste()
