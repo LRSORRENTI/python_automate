9dcf65870daf5d46821f63df8ecce35d3962d36b
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-02-10 09:23:21 -0600

    Added more to section overview of data structures, and start of tictactoe game

--- 07_Dictionaries/09_Data_Structures.py
+++ 07_Dictionaries/09_Data_Structures.py
@@ -1,1 +1,23 @@
 # Data structures in python 
+
+# Lists and dictionaries are useful structures to 
+# house data
+
+dog1 = {'name': 'Buck', 'age': 12, 'color': 'black'}
+dog2 = {'name': 'Jeff', 'age': 7, 'color': 'golden'}
+dog3 = {'name': 'Greg', 'age': 2, 'color': 'crimson'}
+
+allDogs = []
+allDogs.append(dog1)
+allDogs.append(dog2)
+allDogs.append(dog3)
+print(allDogs)
+
+# [{'name': 'Buck', 'age': 12, 'color': 'black'},
+# {'name': 'Jeff', 'age': 7, 'color': 'golden'},
+# {'name': 'Greg', 'age': 2, 'color': 'crimson'}]
+
+# The above is a simple data structure, it's a list 
+# containing dictionaries, this is a way to model data
+# and real world things in a way for python to 
+# understand 
---
+++ 07_Dictionaries/10_TicTacToe.py
@@ -0,0 +1,17 @@
+# TicTacToe 
+
+import pprint
+
+theBoard = {
+    'top-l': ' ',
+    'top-m': ' ',
+    'top-r': ' ',
+    'mid-l': ' ',
+    'mid-m': ' ',
+    'mid-r': ' ',
+    'low-l': ' ',
+    'low-m': ' ',
+    'low-r': ' '
+}
+
+pprint.pprint(theBoard)
